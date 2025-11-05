97162bd2e298e15169e8959bc985c32f0beba482
Christoph Niemann <christoph@dune.com>
2025-11-05 09:45:52 +0100

    Add deployment blocks to triggers
    Signed-off-by: Christoph Niemann <christoph@dune.com>

--- listeners/src/Main.sol
+++ listeners/src/Main.sol
@@ -7,6 +7,9 @@ import "./UniswapV3FactoryListener.sol";
 
 contract Triggers is BaseTriggers {
 
+    uint64 constant UNISWAPV3_FACTORY_ETHEREUM_DEPLOYMENT_BLOCK = 12369621;
+    uint64 constant UNISWAPV3_FACTORY_BASE_DEPLOYMENT_BLOCK = 1371680;
+
     address constant UNISWAPV3_FACTORY_ETHEREUM = 0x1F98431c8aD98523631AE4a59f267346ea31F984;
     address constant UNISWAPV3_FACTORY_UNICHAIN = 0x1F98400000000000000000000000000000000003;
     address constant UNISWAPV3_FACTORY_BASE = 0x33128a8fC17869897dcE68Ed026d694621f6FDfD;
@@ -14,19 +17,21 @@ contract Triggers is BaseTriggers {
     function triggers() external virtual override {
         UniswapV3FactoryListener listener = new UniswapV3FactoryListener();
         addTrigger(
-            chainContract(Chains.Ethereum, UNISWAPV3_FACTORY_ETHEREUM),
+            chainContract(Chains.Ethereum.withStartBlock(UNISWAPV3_FACTORY_ETHEREUM_DEPLOYMENT_BLOCK), UNISWAPV3_FACTORY_ETHEREUM),
             listener.triggerOnCreatePoolFunction()
         );
+
+        // UniswapV3Factory on Unichain is a genesis contract, so it doesn't have a deployment block
         addTrigger(
             chainContract(Chains.Unichain, UNISWAPV3_FACTORY_UNICHAIN),
             listener.triggerOnCreatePoolFunction()
         );
         addTrigger(
-            chainContract(Chains.Base, UNISWAPV3_FACTORY_BASE),
+            chainContract(Chains.Base.withStartBlock(UNISWAPV3_FACTORY_BASE_DEPLOYMENT_BLOCK), UNISWAPV3_FACTORY_BASE),
             listener.triggerOnCreatePoolFunction()
         );
         addTrigger(
-            chainContract(Chains.Ethereum, UNISWAPV3_FACTORY_ETHEREUM),
+            chainContract(Chains.Ethereum.withStartBlock(UNISWAPV3_FACTORY_ETHEREUM_DEPLOYMENT_BLOCK), UNISWAPV3_FACTORY_ETHEREUM),
             listener.triggerOnOwnerChangedEvent()
         );
     }
